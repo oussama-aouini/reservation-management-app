6ca492443cafea802bbdbebadb0e0bc6903bb53d
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2025-09-28 22:45:18 +0100

    add dark mode toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,18 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, IconButton, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import { LuMoon, LuSun } from "react-icons/lu";
+import { useColorMode } from "./ui/color-mode";
 
 const NavBar = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <HStack>
       <Image src={logo} boxSize="30px" />
       <Text>NavBar</Text>
+      <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+        {colorMode === "light" ? <LuSun /> : <LuMoon />}
+      </IconButton>
     </HStack>
   );
 };
