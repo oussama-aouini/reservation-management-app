c1282e30571f7b2ef8941ddebe90d59b9e74d753
Oussama Aouini <oussama.aouini.ext@up.coop>
2025-09-29 13:33:52 +0100

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, IconButton, Image, Text } from "@chakra-ui/react";
+import { HStack, IconButton, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import { LuMoon, LuSun } from "react-icons/lu";
 import { useColorMode } from "./ui/color-mode";
@@ -7,9 +7,8 @@ const NavBar = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="30px" />
-      <Text>NavBar</Text>
       <IconButton onClick={toggleColorMode} variant="outline" size="sm">
         {colorMode === "light" ? <LuSun /> : <LuMoon />}
       </IconButton>
