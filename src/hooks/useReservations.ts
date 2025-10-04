08de0e707fbdbc60e53ca32d235a083032884695
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2025-10-04 20:00:04 +0100

    create a generic data fetching hook

--- src/hooks/useReservations.ts
+++ src/hooks/useReservations.ts
@@ -1,6 +1,4 @@
-import apiClient from "@/services/api-client";
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
+import useData from "./useData";
 
 export interface Reservation {
   id: number;
@@ -8,38 +6,6 @@ export interface Reservation {
   reserver: string;
 }
 
-interface FetchReservationsResponse {
-  count: number;
-  results: Reservation[];
-}
-
-const useReservations = () => {
-  const [reservations, setReservations] = useState<Reservation[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchReservationsResponse>("/reservation", {
-        signal: controller.signal,
-      })
-      .then((res) => {
-        setReservations(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { reservations, error, isLoading };
-};
+const useReservations = () => useData<Reservation>("reservation");
 
 export default useReservations;
